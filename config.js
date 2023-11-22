c159e426d69d260b38f93da87c28f129a1cf4e7b
pedrousus <110744251+pedrousus@users.noreply.github.com>
2023-11-21 20:33:50 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,1 @@
-export const DB_HOST = process.env.DB_HOST || 'shonandeb'
-export const DB_USER = process.env.DB_USER || 'root'
-export const DB_PASSWORD = process.env.DB_PASSWORD || '1234'
-export const DB_NAME = process.env.DB_NAME || 'shonan'
-export const DB_PORT = process.env.DB_PORT || 3306
+
